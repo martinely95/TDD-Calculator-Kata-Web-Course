9556f5724883af80d4c5fef02ea00c02c5fb042d
external.martin.patsov <external.martin.patsov@kuehne-nagel.com>
2020-05-20 18:42:16 +0300

    Refactor add() method.

--- src/main/java/eu/dreamix/Calculator.java
+++ src/main/java/eu/dreamix/Calculator.java
@@ -6,13 +6,11 @@ public class Calculator {
     public int add(String inputString) {
         if (inputString == null || inputString.isEmpty()) {
             return 0;
-        } else if (inputString.contains(",")) {
+        } else {
             String[] tokens = inputString.split(",");
             return Arrays.stream(tokens)
                     .map(Integer::parseInt)
                     .reduce(0, Integer::sum);
-        } else {
-            return Integer.parseInt(inputString);
         }
     }
 }
