9f038194b5234d42fd281b89486d3e8938a60ac5
external.martin.patsov <external.martin.patsov@kuehne-nagel.com>
2020-05-20 18:05:57 +0300

    Add gitignore file. Add first unit test and maven dependencies.

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void newCalculator_shouldBeCreated_whenConstructor_isInvoked() {
+        // given
+
+        // when
+        new Calculator();
+
+        // then
+    }
+
+//    public void add_shouldReturn0_whenEmptyString_isProvided() {
+}
